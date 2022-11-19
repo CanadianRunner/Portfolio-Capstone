67c55556b771c40ef724455f4bc3160aa2303f14
Sean Keane <seankeane93@gmail.com>
2022-11-19 11:40:39 -0800

    added sidebar (deployed) logos and custom color palette

---
+++ portfolio-page/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faAddressCard, faToolbox, faRectangleList, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import '../scss/navbar.scss';
+
+const Navbar = () => (
+  <div className='navbar'>
+    <div className='navbar__logo'>Logo</div>
+    {/* When you get a logo it goes here */}
+    <nav className='navbar__link-group'>
+      <div className='navbar__link'>
+        <span className='navbar__home'>
+          <FontAwesomeIcon icon={faHouse} color="#F6F6C9" size='2x' />
+        </span>
+      </div>
+      <div className='navbar__link'>
+        <span className='navbar__about'>
+          <FontAwesomeIcon icon={faAddressCard} color="#F6F6C9" size='2x' />
+        </span>
+      </div>
+      <div className='navbar__link'>
+        <span className='navbar__skills'>
+          <FontAwesomeIcon icon={faToolbox} color="#F6F6C9" size='2x' />
+        </span>
+      </div>
+      <div className='navbar__link'>
+        <span className='navbar__projects'>
+          <FontAwesomeIcon icon={faRectangleList} color="#F6F6C9" size='2x' />
+        </span>
+      </div>
+      <div className='navbar__link'>
+        <span className='navbar__contact'>
+            <FontAwesomeIcon icon={faEnvelopeOpenText} color="#F6F6C9" size='2x' />
+        </span>
+      </div>
+    </nav>
+    <div className='navbar__github'>GitHUB link</div>
+  </div>
+)
+
+
+export default Navbar;
--- portfolio-page/src/components/nav-bar.js
+++ portfolio-page/src/components/nav-bar.js
@@ -1,9 +1,22 @@
+import '../scss/navbar.scss'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faUser, faEnvelope, faSuitcase, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => (
   <div className='navbar'>
-    
+    {/* When you get a logo it goes here */}
     <nav>
-      
+      <div className='navbar__link'>
+        <span className='navbar__home'>
+          <FontAwesomeIcon icon={faHome} color="#F6F6C9" />
+        </span>
+      </div>
+      <div className='navbar__link'></div>
+      <div className='navbar__link'></div>
+      <div className='navbar__link'></div>
+      <div className='navbar__link'></div>
     </nav>
   </div>
 )
